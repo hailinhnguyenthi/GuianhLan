ac97fff074819a346f0f2c1ae3ad9125728f174b
hailinhnguyenthi <linhnth23416@st.uel.edu.vn>
2025-08-11 14:32:34 +0700

    Change default port to resolve EADDRINUSE error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`App is running at http://localhost:${port}`);
 });
